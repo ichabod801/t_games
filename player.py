57a7c519718c2c64f0a232824350398335196dde
Craig O'Brien <ichabod801@xenomind.com>
2017-08-08 17:16:55 -0400

    Initial (untested) code for bots.

--- player.py
+++ player.py
@@ -11,6 +11,7 @@ Human: A human being, with stored data. (Player)
 from __future__ import print_function
 
 import os
+import string
 import sys
 
 
@@ -61,6 +62,33 @@ class Player(object):
         print(*args, **kwargs)
 
 
+class Bot(Player):
+    """
+    A computer player. (Player)
+
+    Overridden Methods:
+    __init__
+    """
+
+    def __init__(self, taken_names = [], initial = ''):
+        """
+        Set the bot's name. (None)
+
+        If initial is empty, the bot's name can start with any letter.
+
+        Parameters:
+        taken_names: Names already used by a player. (list of str)
+        initial: The first letter of the bot's name. (str)
+        """
+        while True:
+            if initial:
+                self.name = random.choice(BOT_NAMES[initial].split('/'))
+            else:
+                self.name = random.choice(BOT_NAMES[random.choice(string.ascii_lowercase)].split('/'))
+            if self.name not in taken_names:
+                break
+
+
 class Human(Player):
     """
     A human being, with stored data. (Player)
@@ -94,3 +122,30 @@ class Human(Player):
                 print()
             else:
                 break
+
+BOT_NAMES = {'a': 'Ash/Abby/Adam/Alan/Alice/Ada/Adele/Alonzo/Angus/Astro',
+    'b': 'Bender/Barbara/Blue/Bella/Buckaroo/Beth/Bob/Bishop/Betty/Brooke',
+    'c': 'Caitlyn/Calvin/Candice/Carl/Carol/Carsen/Cassandra/Cecilia/Chance/Craig',
+    'd': 'Data/Deckard/Dahlia/Dana/Daphne/Damon/Debby/David/Darryl/Denise',
+    'e': 'Edith/Eve/Ed/Elliot/Elizabeth/Edgar/Enzo/Emelia/Erin/Ernest',
+    'f': 'Felicity/Futura/Fatima/Felix/Felipe/Finn/Fiona/Frances/Frank/Fletcher',
+    'g': 'Gort/Guido/Geneva/Gia/Gerty/Gilberto/Grace/Gloria/Garry/Grover',
+    'h': 'Hymie/Hobbes/Hiro/Homer/Hugo/Haladay/Harriet/Heidi/Hope/Haven',
+    'i': 'Ida/Indira/Ines/Inga/Ivy/Ian/Igor/Isaac/Ichabod/Ivan',
+    'j': 'Judith/Jade/Jane/Jodie/Julia/John/Jaque/Jim/Johan/Jerome',
+    'k': 'Kitt/Karen/Kay/Kelly/Kyoko/Kareem/Kurt/Kronos/Klaus/Kirk',
+    'l': 'Lore/Lee/Lars/Leon/Laszlo/Limor/Laverne/Leelu/Lola/Lois',
+    'm': 'Marvin/Mr. Roboto/MechaCraig/Maximillian/Mordecai/Maria/Mary Lou/Marlyn/Monique/Mika',
+    'n': 'Nellodee/Nancy/Naomi/Norma/Natalie/Nathan/Ned/Nero/Nick/Nigel',
+    'o': 'Otis/Ogden/Omar/Oscar/Otto/Olga/Olivia/Oksana/Octavia/Oriana',
+    'p': 'Pris/Patience/Patty/Phoebe/Pru/Patrick/Phillip/Parker/Paul/Pavel',
+    'q': 'Queenie/Quenby/Quiana/Quinn/Qadir/Qasim/Quincy/Quang/Quest',
+    'r': 'Robby/Roy/Rachel/Risana/Rita/Rosie/River/Reiner/Rick/Rusty',
+    's': 'Sam/Shroud/Santiago/Steve/Sonny/Sarina/Susan/Sylvia/Shirley/Sheba',
+    't': 'Tabitha/Theresa/Tracy/Trinity/Tamala/Tanner/Tariq/Ted/Tyler/Tyrone',
+    'u': 'Ulla/Uma/Ursula/Ursuline/Uta/Ulric/Umberto/Uriah/Usher/Urban',
+    'v': 'Vincent/Valerie/Venus/Vivian/Vera/Veronica/Victor/Viggo/Vikram/Vladimir',
+    'w': 'Wally/Wednesday/Wana/Wendy/Willow/Winnie/Waylon/Wayne/William/Woflgang',
+    'x': 'Xander/Xavier/Xena/Xhosa/Ximena/Xiang/Xaria/Xanthus/Xenon/Xerxes',
+    'y': 'Yamina/Yasmin/Yoland/Yvette/Yadira/Yaakov/Yitzhak/Yves/Yannick/Yaron',
+    'z': 'Zahara/Zelda/Zoe/Zuma/Zenaida/Zachary/Zafar/Zane/Zebulon/Zen'}
