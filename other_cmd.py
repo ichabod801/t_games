9366e806ba8e623aaa8c2ee1fb9b1982c7b88fa1
Craig O'Brien <ichabod801@xenomind.com>
2017-08-11 10:12:54 -0400

    Initial (and tested) code for other_cmd.py.

---
+++ other_cmd.py
@@ -0,0 +1,82 @@
+"""
+other_cmd.py
+
+Basically, a cmd-style command processor without a cmdloop. This is just for
+side handling of other commands during another text input handling loop.
+
+Note that the return values for command handling methods are different in
+OtherCmd compared to Cmd. In OtherCmd, the return value indicates that
+the command was valid, and no warnings for invalid commands need to be
+given to the user.
+
+Classes:
+OtherCmd: An object for handing text commands. (object)
+"""
+
+class OtherCmd(object):
+    """
+    An object for handing text commands. (object)
+
+    Attributes:
+    human: The user of the interface. (player.Player)
+
+    Methods:
+    handle_cmd: Check text input for a valid command. (bool)
+    """
+
+    aliases = {'!': 'debug'}
+
+    def __init__(self, human):
+        """
+        Set up the user. (None)
+
+        Parameters:
+        human: The (ostensibly) human user of the interface. (player.Player)
+        """
+        self.human = human
+
+    def default(self, text):
+        """
+        Handle unrecognized commands. (bool)
+
+        The return value is a flag indicating a valid command. 
+
+        Parameters:
+        text: The raw text input by the user. (str)
+        """
+        return False
+
+    def do_debug(self, arguments):
+        """
+        Handle debugging commands. (bool)
+
+        Parameters:
+        arguments: The debugging information needed. (str)
+        """
+        self.human.tell(eval(arguments))
+        return True
+
+    def handle_cmd(self, text):
+        """
+        Check text input for a valid command. (bool)
+
+        The return value is a flag indicating a valid command. 
+
+        Parameters:
+        text: The raw text input by the user. (str)
+        """
+        command, space, arguments = text.partition(' ')
+        command = command.lower()
+        command = self.aliases.get(command, command)
+        method = 'do_' + command
+        if hasattr(self, method):
+            return getattr(self, method)(arguments)
+        else:
+            return self.default(arguments)
+
+if __name__ == '__main__':
+    import tgames.player
+    other = OtherCmd(tgames.player.Player('Craig'))
+    other.handle_cmd('debug self.aliases')
+    other.handle_cmd('! self.human.name')
+    print(other.handle_cmd('Python sucks.'))
