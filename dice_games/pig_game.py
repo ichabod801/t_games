443cac3a8a94bbe77d834d359379c0da2349cf5d
Craig O'Brien <ichabod801@xenomind.com>
2017-08-12 16:58:27 -0400

    Code/comment review of pig_game.py. Added full_credits.txt, needs implementation.

--- dice_games/pig_game.py
+++ dice_games/pig_game.py
@@ -9,12 +9,21 @@ If you write a new kind of bot, it will not be usable unless you:
     3. Give a list of paramter descriptions as a class attribute.
     4. Put it in the general bots class attribute of the Pig Class.
 
+Constants:
+CREDITS: The credits for the game, progamming, and bots. (str)
+RULES: The rules of Pig. (str)
+SATAN_NAMES: Some names for Satan from the Bible. (str)
+
 Classes:
 PigBot: A bot for playing the game of Pig. (player.Bot)
+PigBotBasePaceRace: A bot w/ min score, max behind, + when to go nuts. (PigBot)
+PigBotPaceRace: A bot with  min score, modifier, and when to go nuts. (PigBot)
+PigBotRolls: A Pig bot that stops after a set number of rolls. (PigBot)
 PigBotScoringTurns: A Pig bot that tries to win in t scoring turns. (PigBot)
 PigBotValue: A Pig bot that rolls until it exceeds a certain value. (PigBot)
-PigBotBasePaceRace: A bot with min score, max behind, + when to go nuts. (PigBot)
+Pig: A game of Pig. (game.Game)
 """
+
 
 from __future__ import print_function
 
@@ -26,13 +35,15 @@ import tgames.player as player
 import tgames.utility as utility
 
 
+# The credits for the game, progamming, and bots.
 CREDITS = """
 Game Design: Traditional
-Game/Bot Programming: "Ichabod" O'Brien
-Bot Design: Roger Johnson, Reiner Knizia, Todd Neller, "Ichabod" O'Brien, 
+Game/Bot Programming: Craig "Ichabod" O'Brien
+Bot Design: Roger Johnson, Reiner Knizia, Todd Neller, Craig O'Brien, 
     Clifton Presser
 """
 
+# The rules of Pig.
 RULES = """
 On your turn, you roll one die. If you roll a one your turn is over and you
 score nothing. Otherwise, you can choose to score what your rolled (ending 
@@ -72,8 +83,11 @@ The preset bots are:
     knizia: A value bot with a value of 20.
     satan: A base-pace-race bot with parameters 6/6/6
     x: A rolls bot with 3 rolls.
+
+The overall default is to have one medium bot.
 """
 
+# Some names for Satan from the Bible.
 SATAN_NAMES = ['Abbadon', 'Apollyon', 'Beast', 'Beelzebub', 'Belial', 'Devil', 'Lucifer', 'Satan']
 
 
@@ -136,10 +150,12 @@ class PigBotBasePaceRace(PigBot):
         Set the bot's value. (None)
 
         Parameters:
-        value: The the minimum turn score this bot will stop rolling on. (int)
+        base: The the minimum turn score this bot will stop rolling on. (int)
+        pace: The maximum score the bot wants to be behind the leader. (int)
+        race: How close the leader can be before it tries to win. (int)
         taken_names: Names already used by a player. (list of str)
         """
-        super(PigBotValue, self).__init__(taken_names, 'b')
+        super(PigBotBasePaceRace, self).__init__(taken_names, 'b')
         self.base = base
         self.pace = pace
         self.race = race
@@ -170,7 +186,7 @@ class PigBotBasePaceRace(PigBot):
 
 class PigBotPaceRace(PigBot):
     """
-    A Pig bot with values for min score, behind modifier, and when to go nuts. (PigBot)
+    A Pig bot with values for min score, modifier, and when to go nuts. (PigBot)
 
     Attributes:
     pace: The the minimum turn score this bot will stop rolling on. (int)
@@ -178,17 +194,19 @@ class PigBotPaceRace(PigBot):
     race: How close the leader can be before it tries to win. (int)
     """
 
-    parameters = ['minimum score', 'behind modifier', 'when to go for it']
+    parameters = ['minimum score', 'score modifier', 'when to go for it']
 
     def __init__(self, pace = 21, modifier = 8, race = 29, taken_names = []):
         """
         Set the bot's value. (None)
 
         Parameters:
-        value: The the minimum turn score this bot will stop rolling on. (int)
+        pace: The the minimum turn score this bot will stop rolling on. (int)
+        modifier: A modifier to pace based how far behind the bot is (int)
+        race: How close the leader can be before it tries to win. (int)
         taken_names: Names already used by a player. (list of str)
         """
-        super(PigBotValue, self).__init__(taken_names, 'p')
+        super(PigBotPaceRace, self).__init__(taken_names, 'p')
         self.pace = pace
         self.modifier = modifier
         self.race = race
@@ -206,7 +224,7 @@ class PigBotPaceRace(PigBot):
         my_score = self.game.scores[self.name]
         hold_value = round(self.pace + (max_other - my_score) / self.modifier, 0)
         # Roll until the base is met.
-        if turn_score < hold_value or my_score + turn_score > 100:
+        if turn_score < hold_value or my_score + turn_score > 99:
             return 'roll'
         # Roll to win if someone is within race.
         elif max_other >= self.race:
@@ -220,7 +238,7 @@ class PigBotRolls(PigBot):
     A Pig bot that stops after a set number of rolls. (PigBot)
 
     Attributes:
-    value: The the minimum turn score this bot will stop rolling on. (int)
+    rolls: The the number of rolls to make. (int)
     """
 
     parameters = ['rolls']
@@ -230,10 +248,10 @@ class PigBotRolls(PigBot):
         Set the bot's value. (None)
 
         Parameters:
-        value: The the minimum turn score this bot will stop rolling on. (int)
+        rolls: The the number of rolls to make. (int)
         taken_names: Names already used by a player. (list of str)
         """
-        super(PigBotValue, self).__init__(taken_names, 'r')
+        super(PigBotRolls, self).__init__(taken_names, 'r')
         self.max_rolls = rolls
         self.rolls = 0
 
@@ -246,8 +264,11 @@ class PigBotRolls(PigBot):
         """
         # Track rolls.
         self.rolls += 1
+        # Calcuate values.
+        turn_score = self.game.turn_score
+        my_score = self.game.scores[self.name]
         # Stop if the number of rolls is met.
-        if self.rolls == self.max_rolls:
+        if self.rolls == self.max_rolls or my_score + turn_score > 99:
             self.rolls = 0
             return 'stop'
         else:
@@ -258,21 +279,27 @@ class PigBotScoringTurns(PigBot):
     """
     A Pig bot that tries to win in t scoring turns. (PigBot)
 
+    For example, with scoring_turns = 4. To start, they will try to score 
+    100 / 4 = 25 points. Say that the first time they score they score 28
+    points. The next time they will try to score (100 - 28) / (4 - 1) = 24 
+    points. If they then score 26 points, they will try to score
+    (100 - 28 - 26) / (4 - 2) = 23 points.
+
     Attributes:
-    value: The the minimum turn score this bot will stop rolling on. (int)
+    scoring_turns: The number of scoring turns to try to win in. (int)
     """
 
     parameters = ['scoring turns']
 
     def __init__(self, scoring_turns = 4, taken_names = []):
         """
-        Set the bot's value. (None)
+        Set the bot's scoring_turns. (None)
 
         Parameters:
-        value: The the minimum turn score this bot will stop rolling on. (int)
+        scoring_turns: The number of scoring turns to try to win in. (int)
         taken_names: Names already used by a player. (list of str)
         """
-        super(PigBotValue, self).__init__(taken_names, 't')
+        super(PigBotScoringTurns, self).__init__(taken_names, 't')
         self.scoring_turns = scoring_turns
 
     def ask(self, prompt):
@@ -320,8 +347,11 @@ class PigBotValue(PigBot):
         Parameters:
         prompt: The question being asked of the player. (str)
         """
+        # Calcuate values.
+        turn_score = self.game.turn_score
+        my_score = self.game.scores[self.name]
         # Stop if the value is met or exceeded.
-        if self.game.turn_score < self.value:
+        if self.game.turn_score < self.value or my_score + turn_score > 99:
             return 'roll'
         else:
             return 'stop'
@@ -334,6 +364,10 @@ class Pig(game.Game):
     Attributes:
     die: The die that is rolled. (dice.Die)
     turn_score: The current player's turn score. (int)
+
+    Methods:
+    ask_options: Get options from the user. (None)
+    parse_options: Parse options from the play command. (None)
 
     Overridden Methods:
     game_over
@@ -352,6 +386,42 @@ class Pig(game.Game):
         'x': (PigBotRolls, (3,))}
     for bot_type, bot_class in general_bots.items():
         preset_bots[bot_type] = (bot_class, ())
+
+    def ask_options(self):
+        """Get options from the user. (None)"""
+        if self.human.ask('Would you like to change the options? ').lower() in utility.YES:
+            # Six is the turn ender.
+            if self.human.ask('Should six be the number that ends the turn? ').lower() in utility.YES:
+                self.bad = 6
+            # Add bots until the user doesn't want any more.
+            while self.human.ask('Would you like to add a bot? ').lower() in utility.YES:
+                # Get the bot type.
+                bot_type = self.human.ask('What type of bot would you like to add? ').lower()
+                # Get the parameters for general bots.
+                if bot_type in self.general_bots:
+                    bot_class = self.general_bots[bot_type]
+                    parameters = []
+                    try:
+                        for parameter in bot_class.parameters:
+                            text = 'What value do you want for the {} parameter? '.format(parameter)
+                            parameters.append(int(self.human.ask(text)))
+                    except ValueError:
+                        self.human.tell('Bot parameters should integers.')
+                    self.players.append(bot_class(*parameters, taken_names = taken_names))
+                    taken_names.append(self.players[-1].name)
+                # Add in preset bots.
+                elif bot_type in self.preset_bots:
+                    bot_class, parameters = self.preset_bots[bot_type]
+                    self.players.append(bot_class(*parameters, taken_names = taken_names))
+                    if word = 'satan':
+                        name = random.choice([name for name in SATAN_NAMES if name not in taken_names])
+                        self.players[-1].name = name
+                    taken_names.append(self.players[-1].name)
+                # Give an infomative warning.
+                else:
+                    self.human.tell("I don't know that kind of bot.")
+                    known_bots = sorted(self.general_bots.keys(), self.present_bots.keys())
+                    self.human.tell('The bots I know are ' + ', '.join(known_bots))
 
     def clean_up(self):
         """Set the loser to go first next round. (None)"""
@@ -378,83 +448,52 @@ class Pig(game.Game):
 
     def handle_options(self):
         """Handle game options and set up players. (None)"""
-        # !! refactor into parse_options vs. ask_options
         # Set the defaults.
         self.players = [self.human]
         self.bad = 1
         # Prep the option loop.
         taken_names = [self.human.name]
         if self.raw_options:
-            # Handle options from play command.
-            for word in self.raw_options().lower().split():
-                # Six is the turn ender.
-                if word == 'six-bad':
-                    self.bad = 6
-                # Check for preset bots.
-                elif word in self.preset_bots:
-                    bot_class, parameters = self.preset_bots[word]
-                    self.players.append(bot_class(*parameters, taken_names = taken_names))
-                    if word = 'satan':
-                        new_name = random.choice([name for name in SATAN_NAMES if name not in taken_names])
-                        self.players[-1].name = new_name
-                    taken_names.append(self.palyers[-1].name)
-                # Check for general bots.
-                elif '=' in word:
-                    bot_type, values = word.split('=')
-                    if bot_type in self.general_bots:
-                        try:
-                            parameters = [int(value) for value in word.split('/')]
-                            new_bot = self.general_bots[bot_type](*parameters, taken_names = taken_names)
-                            self.players.append(new_bot)
-                            taken_names.append(new_bot.name)
-                        except ValueError, TypeError:
-                            self.human.tell('Invalid {} bot specification.'.format(bot_type))
-                    else:
-                        # Warn about unknown bot types.
-                        self.human.tell("I don't know the {} bot.".format(bot_type))
-                else:
-                    # Warn about unknown options.
-                    self.human.tell("I don't recognize the {} option.".format(word))
+            self.parse_options()
         else:
-            # Get options from the user.
-            if self.human.ask('Would you like to change the options? ').lower() in utility.YES:
-                # Six is the turn ender.
-                if self.human.ask('Should six be the number that ends the turn? ').lower() in utility.YES:
-                    self.bad = 6
-                # Add bots until the user doesn't want any more.
-                while self.human.ask('Would you like to add a bot? ').lower() in utility.YES:
-                    # Get the bot type.
-                    bot_type = self.human.ask('What type of bot would you like to add? ').lower()
-                    # Get the parameters for general bots.
-                    if bot_type in self.general_bots:
-                        bot_class = self.general_bots[bot_type]
-                        parameters = []
-                        try:
-                            for parameter in bot_class.parameters:
-                                text = 'What value do you want for the {} parameter? '.format(parameter)
-                                parameters.append(int(self.human.ask(text)))
-                        except ValueError:
-                            self.human.tell('Bot parameters should integers.')
-                        self.players.append(bot_class(*parameters, taken_names = taken_names))
-                        taken_names.append(self.players[-1].name)
-                    # Add in preset bots.
-                    elif bot_type in self.preset_bots:
-                        bot_class, parameters = self.preset_bots[bot_type]
-                        self.players.append(bot_class(*parameters, taken_names = taken_names))
-                        if word = 'satan':
-                            name = random.choice([name for name in SATAN_NAMES if name not in taken_names])
-                            self.players[-1].name = name
-                        taken_names.append(self.players[-1].name)
-                    # Give an infomative warning.
-                    else:
-                        self.human.tell("I don't know that kind of bot.")
-                        known_bots = sorted(self.general_bots.keys(), self.present_bots.keys())
-                        self.human.tell('The bots I know are ' + ', '.join(known_bots))
+            self.ask_options()
         # If no optional bots, default to a basic value bot.
         if self.players == [self.human]:
             self.players.append(PigBotValue(taken_names = [self.human.name]))
         # Shuffle the players.
         random.shuffle(self.players)
+
+    def parse_options(self):
+        """Parse options from the play command. (None)"""
+        for word in self.raw_options().lower().split():
+            # Six is the turn ender.
+            if word == 'six-bad':
+                self.bad = 6
+            # Check for preset bots.
+            elif word in self.preset_bots:
+                bot_class, parameters = self.preset_bots[word]
+                self.players.append(bot_class(*parameters, taken_names = taken_names))
+                if word = 'satan':
+                    new_name = random.choice([name for name in SATAN_NAMES if name not in taken_names])
+                    self.players[-1].name = new_name
+                taken_names.append(self.palyers[-1].name)
+            # Check for general bots.
+            elif '=' in word:
+                bot_type, values = word.split('=')
+                if bot_type in self.general_bots:
+                    try:
+                        parameters = [int(value) for value in word.split('/')]
+                        new_bot = self.general_bots[bot_type](*parameters, taken_names = taken_names)
+                        self.players.append(new_bot)
+                        taken_names.append(new_bot.name)
+                    except ValueError, TypeError:
+                        self.human.tell('Invalid {} bot specification.'.format(bot_type))
+                else:
+                    # Warn about unknown bot types.
+                    self.human.tell("I don't know the {} bot.".format(bot_type))
+            else:
+                # Warn about unknown options.
+                self.human.tell("I don't recognize the {} option.".format(word))
 
     def set_up(self):
         """Set up the game. (None)"""
