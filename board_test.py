d655bf33e23c42d6a5b2076d70de9d71bee01a1c
Craig O'Brien <ichabod801@xenomind.com>
2017-12-25 18:54:17 -0500

    Done with testing of BoardCell. Copy, paste, and adjust for MultiCell.

--- board_test.py
+++ board_test.py
@@ -19,6 +19,21 @@ class BoardCellTest(unittest.TestCase):
     def setUp(self):
         self.cell = board.BoardCell('here', '@')
 
+    def testCapture(self):
+        """Test capturing a piece from a BoardCell."""
+        capture = self.cell.add_piece('&')
+        self.assertEqual('@', capture)
+
+    def testClear(self):
+        """Test clearing a BoardCell."""
+        self.cell.clear()
+        self.assertIsNone(self.cell.contents)
+
+    def testClearSpecific(self):
+        """Test clearing a BoardCell with a specified nothing."""
+        self.cell.clear(' ')
+        self.assertEqual(' ', self.cell.contents)
+
     def testContains(self):
         """Test the in operator for BoardCell."""
         self.assertTrue('@' in self.cell)
@@ -26,6 +41,20 @@ class BoardCellTest(unittest.TestCase):
     def testContainsNot(self):
         """The failure of the in operator for BoardCell."""
         self.assertFalse('p' in self.cell)
+
+    def testCopyIndependence(self):
+        """Test that the copied piece is independent of the BoardCell piece."""
+        piece = self.cell.copy_piece()
+        self.cell.add_piece('&')
+        self.assertNotEqual(piece, self.cell.contents)
+
+    def testCopyValue(self):
+        """Test that copy_piece gives the correct value."""
+        self.assertEqual('@', self.cell.copy_piece())
+
+    def testGetPiece(self):
+        """Test getting a piece from the cell."""
+        self.assertEqual('@', self.cell.get_piece())
 
     def testHash(self):
         """Test for the expected hash of a BoardCell."""
@@ -54,6 +83,15 @@ class BoardCellTest(unittest.TestCase):
         self.cell.contents = None
         self.assertEqual(0, len(self.cell))
 
+    def testRemoveContents(self):
+        """Test BoardCell contents after removing a piece."""
+        piece = self.cell.remove_piece()
+        self.assertIsNone(self.cell.contents)
+
+    def testRemoveReturn(self):
+        """Test that removing a piece returns the correct value."""
+        self.assertEqual('@', self.cell.remove_piece())
+
     def testRepr(self):
         """Test the repr of a BoardCell."""
         self.assertEqual("BoardCell('here', piece = '@')", repr(self.cell))
@@ -64,9 +102,23 @@ class BoardCellTest(unittest.TestCase):
         self.assertEqual("BoardCell('here')", repr(self.cell))
 
     def testReprDesign(self):
-        """Test the repr of a BoardCell."""
+        """Test the repr of a BoardCell with a board design."""
         self.cell.empty = '+'
         self.assertEqual("BoardCell('here', piece = '@', empty = '+')", repr(self.cell))
+
+    def testPlace(self):
+        """Test adding a piece to a Board cell."""
+        self.cell.add_piece('&')
+        self.assertEqual('&', self.cell.contents)
+
+    def testStr(self):
+        """Test the string of a BoardCell."""
+        self.assertEqual('@', str(self.cell))
+
+    def testStrEmpty(self):
+        """Test the string of an empty BoardCell."""
+        self.cell.contents = None
+        self.assertEqual(' ', str(self.cell))
 
 
 if __name__ == '__main__':
