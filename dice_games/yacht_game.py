0a882497041a4bc1c8972cc2bebfdf6f81b8ff42
Craig O'Brien <ichabod801@xenomind.com>
2018-02-10 17:58:35 -0500

    More yacht_game.py programming. Started on Bacht, a simple bot to test the game functionality with.

--- dice_games/yacht_game.py
+++ dice_games/yacht_game.py
@@ -5,6 +5,7 @@ Games similar to Yacht.
 
 Classes:
 ScoreCategory: A category for a category dice game. (object)
+Bacht: A bot to play Yacht. (player.Bot)
 Yacht: The game of Yacht and it's cousins. (game.Game)
 
 Functions:
@@ -18,6 +19,7 @@ straight: Score a straight category. (int)
 
 import tgames.dice as dice
 import tgames.game as game
+import tgames.player as player
 
 
 class ScoreCategory(object):
@@ -68,43 +70,11 @@ class ScoreCategory(object):
         else:
             self.bonus = 0
 
-    def do_hold(self, arguments):
-        """
-        Hold back dice for scoring. (bool)
-
-        Parameters:
-        arguments: The dice (values) to hold. (str)
-        """
-        # Get the current player.
-        player = self.players[self.player_index]
-        # Get the integer values to hold.
-        try:
-            holds = [int(word) for word in arguments.split()]
-        except ValueError:
-            player.error('Invalid argument to hold: {!r}.'.format(arguments))
-        # Hold the dice.
-        try:
-            self.dice.hold(holds)
-        except ValueError:
-            player.error('You do not have all of those dice to hold.')
-        return True
-
-    def do_roll(self, arguments):
-        """
-        Roll the dice (excluding any held back). (bool)
-
-        Parameters:
-        arguments: The dice (values) to hold. (str)
-        """
-        # Get the current player.
-        player = self.players[self.player_index]
-        # Check the roll count
-        if self.roll_count == 3:
-            player.error('You have already rolled three times.')
-        else:
-            # Roll the dice and mark the roll.
-            self.dice.roll()
-            self.roll_count += 1
+    def copy(self):
+        """Create an independent copy of the category. (ScoreCategory)"""
+        clone = ScoreCategory(self.name, self.description, self.validator, self.score_type, self.first)
+        clone.bonus = self.bonus
+        return clone
 
     def score(self, dice, roll_count):
         """
@@ -198,6 +168,23 @@ def straight(dice):
     return 0
 
 
+class Bacht(player.Bot):
+    """
+    A bacht to play Yacht. (player.Bot)
+
+    This is just a dummy bot to test the game functionality.
+    """
+
+    def ask(self, query):
+        """
+        Ask the bacht a question. (str)
+
+        Parameters:
+        query: The question to ask of the bacht. (str)
+        """
+        pass # !! not finished
+
+
 class Yacht(game.Game):
     """
     The game of Yacht and it's cousins. (game.Game)
@@ -206,14 +193,22 @@ class Yacht(game.Game):
     categories: The scoring functions for each category. (dict of str: callable)
 
     Attributes:
+    category_scores: The player's scores in each category. (dict of str: dict)
     dice: The pool of dice for the game. (dice.Pool)
 
+    Methods:
+    do_hold: Hold back dice for scoring. (bool)
+    do_roll: Roll the dice (excluding any held back). (bool)
+    do_score: Score the current dice roll. (bool)
+
     Overridden Methods:
+    game_over
+    handle_options
+    player_action
     set_up
     """
 
-    # !! display should come from here, but where to store order? OrderedDict?
-    categories = {'ones': ScoreCategory('Ones', 'As many ones as possible', score_n(1)),
+    categories = ['ones': ScoreCategory('Ones', 'As many ones as possible', score_n(1)),
         'twos': ScoreCategory('Twos', 'As many twos as possible', score_n(2)),
         'threes': ScoreCategory('Threes', 'As many threes as possible', score_n(3)),
         'fours': ScoreCategory('Fours', 'As many fours as possible', score_n(4)),
@@ -221,16 +216,115 @@ class Yacht(game.Game):
         'sixes': ScoreCategory('Sixes', 'As many sixes as possible', score_n(6)),
         'full-house': ScoreCategory('Full House', 'Three of a kind and a pair', full_house),
         'four-kind': ScoreCategory('Four of a Kind', 'Four of the same number', four_kind),
-        'litte-straight': ScoreCategory('Little Striaght', '1-2-3-4-5', straight, '30'),
+        'litte-straight': ScoreCategory('Little Straight', '1-2-3-4-5', straight, '30'),
         'big-straight': ScoreCategory('Big Straight', '2-3-4-5-6', straight, '30'),
         'choice': SubCategory('Choice', 'Any roll', lambda dice: 1, 'total'),
-        'yacht': SubCategory('Yacht', 'Five of the same number', five_kind, '50')}
+        'yacht': SubCategory('Yacht', 'Five of the same number', five_kind, '50')]
+
+    def do_hold(self, arguments):
+        """
+        Hold back dice for scoring. (bool)
+
+        Parameters:
+        arguments: The dice (values) to hold. (str)
+        """
+        # Get the current player.
+        player = self.players[self.player_index]
+        # Get the integer values to hold.
+        try:
+            holds = [int(word) for word in arguments.split()]
+        except ValueError:
+            player.error('Invalid argument to hold: {!r}.'.format(arguments))
+        # Hold the dice.
+        try:
+            self.dice.hold(holds)
+        except ValueError:
+            player.error('You do not have all of those dice to hold.')
+        return True
+
+    def do_roll(self, arguments):
+        """
+        Roll the dice (excluding any held back). (bool)
+
+        Parameters:
+        arguments: The dice (values) to hold. (str)
+        """
+        # Get the current player.
+        player = self.players[self.player_index]
+        # Check the roll count
+        if self.roll_count == 3:
+            player.error('You have already rolled three times.')
+        else:
+            # Roll the dice and mark the roll.
+            self.dice.roll()
+            self.roll_count += 1
+
+    def do_score(self, arguments):
+        """
+        Score the current dice roll. (bool)
+
+        Parameters:
+        arguments: The category to score the roll in. (str)
+        """
+        # Get the current player.
+        player = self.players[self.player_index]
+        # Find the correct category.
+        # !! It would be good to provide some flexibility in identifying categories.
+        for category in self.categories:
+            if arguments.lower() == category.name.lower():
+                break
+        else:
+            # Handle unknown categories.
+            player.error('I do not recognize that category.')
+            known = [category.name for category in self.categories]
+            player.error('The categories I know are: {}.'.format(', '.join(known)))
+            return False
+        # Score the roll in that category.
+        score = category.score(self.dice, self.roll_count)
+        if self.category_scores[player.name][category.name] is None:
+            self.category_scores[player.name][category.name] = score
+            self.scores[player.name] += score
+            # Reset the dice for the next player.
+            self.roll_count = 1
+            self.dice.release()
+            self.dice.roll()
+            return True
+        else:
+            # Handle previously scored categories.
+            player.error('You have already scored in that category.')
+            return False
+
+    def game_over(self):
+        """Check for all categories having been used. (bool)"""
+        count = sum([list(player.score.values()).count(None) for player in self.players])
+        if not count:
+            self.category_scores = self.scores
+            for player_name in self.category_scores:
+                self.scores[player_name] = sum(self.category_scores[player_name].values())
+        return count == 0
 
     def handle_options(self):
         """Handle the game options."""
         super(Yacht, self).handle_options()
-        self.categories = Yacht.categories.copy()
+        self.categories = [category.copy() for category in Yacht.categories]
+
+    def player_action(self, player):
+        """
+        Handle a player's action during their turn. (bool)
+
+        Parameters:
+        player: The player whose turn it is. (player.Player)
+        """
+        # Show the game status.
+        player.tell('\nThe roll to you is {}.'.format(self.dice))
+        player.tell('You have {} rerolls left.\n'.format(3 - self.roll_count))
+        # Get the player's move.
+        move = player.ask('What is your move? ')
+        return self.handle_cmd(move)
 
     def set_up(self):
         """Set up the game. (None)"""
         self.dice = dice.Pool([6] * 5)
+        score_base = {category.name: None for category in self.categories}
+        self.category_scores = {player.name: score_base.copy() for player in self.players}
+        self.scores = {player.name: 0 for player in self.players}
