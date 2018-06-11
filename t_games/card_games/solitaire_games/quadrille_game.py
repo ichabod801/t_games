ce689368d75ad58d12182ee3fadbcbbc74c21588
Craig O'Brien <ichabod801@xenomind.com>
2018-06-11 13:48:22 -0400

    Issue #7: Fixed Quadrille's output of itself.
    It wasn't putting a space before the layout. I also snuck in the full_auto
    method, which plays the game for you.

--- t_games/card_games/solitaire_games/quadrille_game.py
+++ t_games/card_games/solitaire_games/quadrille_game.py
@@ -8,6 +8,8 @@ Quadrille: A game of Quadrille. (solitaire.Solitaire)
 """
 
 
+import time
+
 import t_games.card_games.solitaire_games.solitaire_game as solitaire
 
 
@@ -21,19 +23,32 @@ class Quadrille(solitaire.Solitaire):
     # The name of the game.
     name = 'Quadrille'
 
+    def do_auto(self, arguments):
+        """
+        Automatically play cards into the foundations. (bool)
+        
+        Parameters:
+        arguments: The arguments to the sort command. (str)
+        """
+        # Check for full auto.
+        if arguments.lower().startswith('f'):
+            self.full_auto(arguments)
+            return False
+        # Otherwise handle normally.
+        else:
+            return super(Quadrille, self).do_auto(arguments)
+
     def __str__(self):
         """Human readable text representation. (str)"""
         piles = self.foundations
-        lines = ['      {}'.format(piles[2][-1])]
+        lines = ['', '      {}'.format(piles[2][-1])]
         lines.append('  {}      {}'.format(piles[5][-1], piles[6][-1]))
         lines.append('      QH')
         lines.append('{}  QD  QS  {}'.format(piles[1][-1], piles[3][-1]))
         lines.append('      QC')
         lines.append('  {}      {}'.format(piles[4][-1], piles[7][-1]))
         lines.append('      {}'.format(piles[0][-1]))
-        lines.append('')
-        lines.append(self.stock_text())
-        lines.append('')
+        lines.extend(('', self.stock_text(), ''))
         return '\n'.join(lines)
 
     def find_foundation(self, card):
@@ -47,6 +62,47 @@ class Quadrille(solitaire.Solitaire):
         if card.rank in '789TJ':
             foundation_index += 4
         return self.foundations[foundation_index]
+
+    def full_auto(self, arguments):
+        """
+        Automatically play the game. (bool)
+        
+        Parameters:
+        arguments: The arguments to the sort command. (str)
+        """
+        # Strip out non-digits from the front of the argument.
+        while arguments and not arguments.isdigit():
+            arguments = arguments[1:]
+        # Convert the arguments to a 0-10 digit.
+        if arguments:
+            speed = 10 - min(10, max(0, int(arguments)))
+        else:
+            speed = 5
+        # Make moves while there are cards to move.
+        passes = self.stock_passes
+        while self.stock or self.waste:
+            # Get a card to sort.
+            if not self.waste:
+                self.do_turn('')
+            else:
+                # Check the card for sorting.
+                foundation = self.find_foundation(self.waste[-1])
+                if self.sort_check(self.waste[-1], foundation, False):
+                    self.do_sort(str(self.waste[-1]))
+                # If you cant sort, get another card.
+                elif self.stock:
+                    self.do_turn('')
+                else:
+                    # Keep track of passes through the deck, and exit when limit reached.
+                    passes += 1
+                    if passes == self.options['max-passes']:
+                        break
+                    else:
+                        self.do_turn('')
+            # Update tracking and pause.
+            print(self)
+            self.turns += 1
+            time.sleep(0.1 * speed)
 
     def set_checkers(self):
         """Set the game specific rule checking functions. (None)"""
