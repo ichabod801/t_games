7ff7ee845439b7c239dd3c87e4770697559ab636
Craig O'Brien <ichabod801@xenomind.com>
2018-06-19 16:50:09 -0400

    Issue #64: Separated LiarsDice end of game text from numbers.
    I changed before to a str, but it needed to be an int for win_loss_draw. So I
    changed the variable name of the str version.

--- t_games/dice_games/liars_dice_game.py
+++ t_games/dice_games/liars_dice_game.py
@@ -464,8 +464,8 @@ class LiarsDice(game.Game):
             # Announce the loss.
             self.human.tell('\nYou have no more tokens, you lose the game.')
             before = len([player for player in self.players if not self.scores[player.name]]) - 1
-            before = number_word(before).capitalize()
-            self.human.tell('{} players left the game before you did.'.format(before))
+            before_text = number_word(before).capitalize()
+            self.human.tell('{} players left the game before you did.'.format(before_text))
             # Record the win/loss/draw.
             self.win_loss_draw = [before, len(self.players) - before - 1, 0]
         # Check for the human being the only one left.
