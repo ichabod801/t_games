30f194746383fd6c1fe893b0a6e6b8b4d6cadc97
Craig O'Brien <ichabod801@xenomind.com>
2018-10-21 14:37:56 -0400

    No Issue: Editted full credits to shorten an overly long name.

--- t_games/full_credits.py
+++ t_games/full_credits.py
@@ -17,7 +17,7 @@ data_to_text: Convert CREDITS_DATA to formatted text. (str)
 
 CREDITS_DATA = [('Interface Programming', """Craig "Ichabod" O'Brien"""),
     ('Game Design', """Humans; Paul Alfille, Richard A. Canfield, Doug Dyment, Michael Keller,
-        Bernard X. de Marigny, Albert Morehead, Geoffrey Mott-Smith, Ned Strongin, Sid Sackson,
+        Bernard de Marigny, Albert Morehead, Geoffrey Mott-Smith, Ned Strongin, Sid Sackson,
         John Suckling, Howard Wexler, Gregory Yob"""),
     ('Game Programming', """Craig "Ichabod" O'Brien"""),
     ('Bot Design', """Roger Johnson, Reiner Knizia, Todd Neller, Craig O'Brien, Clifton Presser,
