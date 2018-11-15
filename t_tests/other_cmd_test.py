697ae73cce68be61c2d3f8dc87798606fb63406f
Craig O'Brien <ichabod801@xenomind.com>
2018-11-15 17:09:37 -0500

    Issue #234: Added tests of OtherCmd.do_debug.

--- t_tests/other_cmd_test.py
+++ t_tests/other_cmd_test.py
@@ -4,7 +4,8 @@ other_cmd_test.py
 Unit testing of t_games/other_cmd.py.
 
 Classes:
-OtherCmdTest: Tests of a text command handler. (unittest.TestCase)
+OtherCmdDebugTest: Tests of debugging with a text command handler. (TestCase)
+OtherCmdTextTest: Tests of text for a command handler. (unittest.TestCase)
 """
 
 
@@ -12,9 +13,39 @@ import unittest
 
 import t_games.other_cmd as other_cmd
 import t_games.player as player
+import t_tests.unitility as unitility
 
 
-class OtherCmdTest(unittest.TestCase):
+class OtherCmdDebugTest(unittest.TestCase):
+	"""Tests of debugging with a text command handler. (unittest.TestCase)"""
+
+	def setUp(self):
+		self.bot = unitility.AutoBot()
+		self.cmd_handler = other_cmd.OtherCmd(self.bot)
+
+	def testBasic(self):
+		"""Test a basic debugging."""
+		self.cmd_handler.do_debug('1 ** 1 * 2 ** 2 * 3 ** 3')
+		self.assertEqual('108\n', self.bot.info[0])
+
+	def testException(self):
+		"""Test the debug command catching an exception."""
+		self.cmd_handler.do_debug('1 / 0')
+		check = '\nThere was an exception raised while processing that command:\n'
+		self.assertEqual(check, self.bot.errors[0])
+
+	def testSelf(self):
+		"""Test that self in debugging references the other_cmd instance."""
+		self.cmd_handler.do_debug('id(self)')
+		self.assertEqual(str(id(self.cmd_handler)), self.bot.info[0].strip())
+
+	def testTurnContinuation(self):
+		"""Test that debugging does not end the turn."""
+		self.cmd_handler.do_debug('1 ** 1 * 2 ** 2 * 3 ** 3')
+		self.assertTrue(self.bot.info[0])
+
+
+class OtherCmdTextTest(unittest.TestCase):
     """Tests of a text command handler. (unittest.TestCase)"""
 
     def setUp(self):
