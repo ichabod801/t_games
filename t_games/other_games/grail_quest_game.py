b5802f5cf06012e74330f6079fc37d3fcc64d13e
Craig O'Brien <ichabod801@xenomind.com>
2018-12-03 17:02:04 -0500

    Issue #239: Worked on stopping at a castle.

--- t_games/other_games/grail_quest_game.py
+++ t_games/other_games/grail_quest_game.py
@@ -3,6 +3,36 @@ grail_quest_game.py
 
 An Oregon Trail style game based on Monty Python and the Holy Grail.
 
+4902  REM A = AMOUNT SPENT ON ANIMALS
+4904  REM B = AMOUNT SPENT ON AMMUNITION
+4906  REM B1 = ACTUAL RESPONSE TIME FOR INPUTING 'BANG'
+4908  REM B2 = MAXIMUM RESPONSE TIME FOR INPUTING 'BANG'
+4910  REM C = AMOUNT SPENT ON CLOTHING
+4912  REM C1 = FLAG FOR INSUFFICIENT CLOTHING IN COLD WEATHER
+4914  REM C$ = YES/NO RESPONSE TO QUESTIONS
+4916  REM D1 = COUNTER IN GENERATING EVENTS
+4918  REM D3 = TURN NUMBER FOR SETTING DATE
+4920  REM D4 = CURRENT DATE
+4922  REM E = CHOICE OF EATING
+4924  REM F = AMOUNT SPENT ON FOOD
+4926  REM F1 = FLAG FOR CLEARING SOUTH PASS
+4928  REM F2 = FLAG FOR CLEARING BLUE MOUNTAINS
+4930  REM F9 = FRACTION OF 2 WEEKS TRAVELED ON FINAL TURN
+4932  REM K8 = FLAG FOR INJURY
+4934  REM L1 = FLAG FOR BLIZZARD
+4936  REM M = TOTAL MILEAGE WHOLE TRIP
+4938  REM M1 = AMOUNT SPENT ON MISCELLANEOUS SUPPLIES
+4940  REM M2 = TOTAL MILEAGE UP THROUGH PREVIOUS TURN
+4942  REM M9 = FLAG FOR CLEARING SOUTH PASS IN SETTING MILEAGE
+4944  REM P = AMOUNT SPENT ON ITEMS AT FORT
+4946  REM R1 = RANDOM NUMBER IN CHOOSING EVENTS
+4948  REM S4 = FLAG FOR ILLNESS
+4950  REM S5 = 'HOSTILITY OF RIDERS' FACTOR
+4952  REM T = CASH LEFT OVER AFTER INITIAL PURCHASES
+4954  REM T1 = CHOICE OF TACTICS WHEN ATTACKED
+4956  REM X = CHOICE OF ACTION FOR EACH TURN
+4958  REM X1 = FLAG FOR FORT OPTION
+
 Constants:
 CREDITS: The credits for the game. (str)
 FINAL_CREDITS: You know, the staff need time to clean the place. (str)
@@ -146,10 +176,13 @@ class GrailQuest(game.Game):
     """
 
     aka = ['I seek the Holy Grail!', 'qftg']
+    aliases = {'castle': 'stop', 's': 'stop'}
     categories = ['Adventure Games']
+    diseases = ['measles', 'dysentery', 'typhoid', 'cholera', 'spamitis']
     credits = CREDITS
     credits_order = [CREDITS, MOOSE_CREDITS, SACKED_CREDITS, SACKER_CREDITS, MOOSER_CREDITS,
         SACKEST_CREDITS, LLAMA_CREDITS, FINAL_CREDITS]
+    forts = ['Castle of Camelot', 'Swamp Castle', 'Castle Anthrax', 'Spam Castle', 'Catapult Castle']
     name = 'Quest for the Grail'
 
     def do_credits(self):
@@ -165,34 +198,54 @@ class GrailQuest(game.Game):
             self.force_end = 'loss'
             return False
 
-    def initial_purchases(self):
-        """Make the initial purchases. (None)"""
-        print(INITIAL_PURCHASES)
-        # Purchase steeds.
-        query = 'How much would you like to spend on steeds? '
-        self.steeds = self.human.ask_int(query, min = 180, max = 270, cmd = False)
-        self.gold -= self.steeds
+    def do_stop(self):
+        """
+        Stop at a castle to buy things. (s, castle)
+        """
+        if not self.fort_option:
+            self.human.tell('The castle you thought you could get to turned out to only be a model.')
+            return True
+        else:
+            self.human.tell('\nWelcome to {}!'.format(self.castles))
+            modifier = random.random() / 2 + 0.5
+            self.purchases(modifier)
+            self.mileage -= 45
+            return False
+
+    def purchases(self, modifier = None):
+        """Make purchases. (None)"""
+        if modifier is None:
+            self.human.tell(INITIAL_PURCHASES)
+            modifier = 1
+            # Purchase steeds.
+            query = 'How much would you like to spend on steeds? '
+            self.steeds = self.human.ask_int(query, min = 180, max = 270, cmd = False)
+            self.gold -= self.steeds
         # Purchase coconuts.
         query = 'How much would you like to spend on coconuts? '
-        self.coconuts = self.human.ask_int(query, min = 20, max = 30, cmd = False)
-        self.gold -= self.coconuts
+        new_coconuts = self.human.ask_int(query, min = 20, max = 30, cmd = False)
+        self.gold -= new_coconuts
+        self.coconuts += int(new_coconuts * modifier)
         # Purchase spam.
         query = 'How much would you like to spend on spam? '
-        self.spam = self.human.ask_int(query, min = 0, max = self.gold, cmd = False)
-        self.gold -= self.spam
+        new_spam = self.human.ask_int(query, min = 0, max = self.gold, cmd = False)
+        self.gold -= new_spam
+        self.spam += int(new_spam * modifier)
         # Purchase arrows.
         query = 'How much would you like to spend on arrows? '
-        self.arrows = self.human.ask_int(query, min = 0, max = self.gold, cmd = False)
-        self.gold -= self.arrows
-        self.arrows *= 50
+        new_arrows = self.human.ask_int(query, min = 0, max = self.gold, cmd = False)
+        self.gold -= new_arrows
+        self.arrows += int(new_arrows * modifier * 50)
         # Purchase clothing.
         query = 'How much would you like to spend on clothing? '
-        self.clothing = self.human.ask_int(query, min = 0, max = self.gold, cmd = False)
-        self.gold -= self.clothing
+        new_clothing = self.human.ask_int(query, min = 0, max = self.gold, cmd = False)
+        self.gold -= new_clothing
+        self.clothing += int(new_clothing * modifier)
         # Purchase miscellaneous supplies.
         query = 'How much would you like to spend on miscellaneous supplies? '
-        self.miscellaneous = self.human.ask_int(query, min = 0, max = self.gold, cmd = False)
-        self.gold -= self.miscellaneous
+        new_miscellaneous = self.human.ask_int(query, min = 0, max = self.gold, cmd = False)
+        self.gold -= new_miscellaneous
+        self.miscellaneous = int(new_miscellaneous * modifier)
         # Inform the player of the balance.
         self.human.tell('After all of your purchases, you have {} pieces of gold left.'.format(self.gold))
 
@@ -205,18 +258,37 @@ class GrailQuest(game.Game):
         """
         # Check for initial purchases.
         if not self.turns:
-            self.initial_purchases()
+            self.purchases()
         # Update the date.
         self.human.tell('\n{}'.format(self.date).replace(' 0', ' '))
-        # Make any necessary alerts.
+        # Update the user.
         if self.spam < 12:
             self.human.tell('You better do some hunting or buy food soon!')
         if self.coconuts < 11:
             self.human.tell('Your steeds have just about beaten those coconuts to death.')
         if self.fort_option:
             self.human.tell('You can see a castle in the distance.')
-        # Update the human and get the action.
         self.show_status()
+        # Check for medical issues.
+        if self.illness and self.injury:
+            bill = 40
+        elif self.illness or self.injury:
+            bill = 20
+        if bill:
+            warnings.append("The apothecary's bill is {} gold.".format(bill))
+            self.injury = False
+            self.illness = False
+        if warnings:
+            self.human.tell('', '\n'.join(warnings))
+        if bill > self.gold:
+            if self.illness:
+                self.death = 'illness'
+            else:
+                self.death = 'injury'
+            return False
+        else:
+            self.gold -= bill
+        # Get the player's action.
         action = self.human.ask('\nWhat would you like to do? ')
         go = self.handle_cmd(action)
         if not go and not self.force_end:
@@ -236,12 +308,18 @@ class GrailQuest(game.Game):
         self.date = datetime.date(932, 4, 12)
         self.fortnight = datetime.timedelta(days = 14)
         self.fort_option = True
+        self.fort_index = 0
         self.gold = 700
         self.mileage = 0
+        self.illness = False
+        self.injury = False
+        self.death = ''
+        self.felt_better = False
 
     def show_status(self):
         """Show the current consumables. (None)"""
-        self.human.tell('You have travelled {} miles.'.format(self.mileage))
+        self.human.tell('\nToday is {}.'.format(self.date))
+        self.human.tell('\nYou have travelled {} miles.'.format(self.mileage))
         self.human.tell('You have:')
         self.human.tell('{} cans of spam,'.format(self.spam))
         self.human.tell('{} arrows,'.format(self.arrows))
