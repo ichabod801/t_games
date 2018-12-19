178ab9dcbf088ad731e27c27de6f1e74f368697f
Craig O'Brien <ichabod801@xenomind.com>
2018-12-19 13:44:08 -0500

    Issue #261: Added tests of ninety_nine_game.py, with bug fixes.
    Fixes included adding a default to Bot99.tell, and cleaning up the option
    groups specifying no rank for an option (including the documentation).

---
+++ t_tests/ninety_nine_test.py
@@ -0,0 +1,35 @@
+"""
+ninety_nine_test.py
+
+Automated testing of ninety_nine_game.py.
+
+Constants:
+TEST_BOTS: The list of bots for bot testing. (list of Bot99)
+
+Classes:
+Bot99ChicagoTest: Tests of Ninety Nine / Chicago. (unitest.TestCase)
+Bot99JokerTest: Tests of Ninety Nine / joker-rules. (unitest.TestCase)
+Bot99Test: Tests of Ninety Nine with no options. (unitest.TestCase)
+"""
+
+
+import unittest
+
+import t_games.card_games.ninety_nine_game as ninety9
+import t_tests.unitility as unitility
+
+
+TEST_BOTS = [ninety9.Bot99, ninety9.Bot99Medium] * 3
+
+
+Bot99ChicagoTest = unitility.bot_test(ninety9.NinetyNine, TEST_BOTS, 4, [3, 4, 5, 6], 'chicago')
+
+
+Bot99JokerTest = unitility.bot_test(ninety9.NinetyNine, TEST_BOTS, 4, [3, 4, 5, 6], 'joker-rules')
+
+
+Bot99Test = unitility.bot_test(ninety9.NinetyNine, TEST_BOTS, 4, [3, 4, 5, 6])
+
+
+if __name__ == '__main__':
+    unittest.main()
