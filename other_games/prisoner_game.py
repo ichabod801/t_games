9e0295bbe25c8393640d6c6f4c52f9d0005f25b5
Craig O'Brien <ichabod801@xenomind.com>
2019-02-25 00:20:46 -0500

    Issue #288: Finished initial coding of the basic game play.

--- other_games/prisoner_game.py
+++ other_games/prisoner_game.py
@@ -11,7 +11,11 @@ Classes:
 PrisonersDilemma: A game of the Interated Prisoner's Dilemma. (game.Game)
 """
 
+
+import itertools
+
 from .. import game
+
 
 CREDITS = """
 Game Design: Merrill Flood and Melvin Dresher
@@ -39,6 +43,7 @@ get you the best score no matter what your opponent does. However, mutual
 cooperation is a better outcome (the total score is highest).
 
 Options:
+iterations= (i=): The number of rounds played (defaults to 10).
 punishment= (p=): The punishment score. It must be higher than the sucker bet.
 reward= (r=): The reward score. It must be higher than the punishment score.
 sucker= (s=): The sucker bet score. It must be lower than the punishment score.
@@ -46,20 +51,78 @@ temptation= (t=): The temptation score. It must be higher than the reward
     score.
 """
 
+
 class PrisonersDilemma(game.Game):
     """
     A game of the Interated Prisoner's Dilemma. (game.Game)
     """
 
     credits = CREDITS
+    move_aliases = {'c': 'cooperate', 'd': 'defect'}
     num_options = 4
     rules = RULES
 
+    def game_over(self):
+        """Check for the end of the game. (bool)"""
+        return self.turns == self.num_turns
+
     def handle_options(self):
         """Handle the game options from the user. (None)"""
-        self.points = {}
         super(PrisonersDilemma, self).handle_options(self)
+
+    def player_action(self, player):
+        """
+        Run one round of the player's dilemma. (None)
+
+        This doesn't run one player's action, it runs one iteration of the Iterated
+        Prisoner's Dilemma. That is, it does the Prisoner's Dilemma for every pair of
+        players.
+        """
+        # Check each pair of players.
+        for sub_players in itertools.combinations(self.players, 2):
+            # Get the moves
+            moves = []
+            queries = ['What is your move against {}? '.format(player.name) for player in sub_players]
+            queries.reverse()
+            for player, query in zip(sub_players, queries):
+                while True:
+                    move = player_1.ask(query).lower()
+                    move = move_aliases.get(move_1, move_1)
+                    if move in ('cooperate', 'defect'):
+                        break
+                    else:
+                        self.handle_cmd(move_1)
+            moves.append(move)
+            # Get the scoring results.
+            if moves == ['cooperate', 'cooperate']:
+                round_scores = [self.points['punishment'], self.points['punishment']]
+            elif moves == ['cooperate', 'defect']:
+                round_scores = [self.points['temptation'], self.points['sucker']]
+            elif moves == ['defect', 'cooperate']:
+                round_scores = [self.points['sucker'], self.points['temptation']]
+            elif moves == ['defect', 'defect']:
+                round_scores = [self.points['reward'], self.points['reward']]
+            # Score the points and inform the players
+            for player, round_score in zip(sub_players, round_scores):
+                self.scores[player.name] += round_score
+                foe_index = 1 - sub_players.index(player)
+                foe_name = sub_players[foe_index].name
+                foe_move = moves[foe_index]
+                player.tell('{} chose to {}.'.format(foe_name, foe_move))
+        return False
 
     def set_options(self):
         """Set the possible game options. (None)"""
-        self.option_set.add_option('sucker', ['s'], int, default = 0)
+        self.points = {}
+        self.option_set.add_option('sucker', ['s'], int, default = 0, action = 'key=sucker',
+            target = self.points,
+            question = 'How much should the sucker bet be worth (return for 0)? ')
+        self.option_set.add_option('sucker', ['s'], int, default = 0, action = 'key=sucker',
+            target = self.points,
+            question = 'How much should the punishment be worth (return for 1)? ')
+        self.option_set.add_option('sucker', ['s'], int, default = 0, action = 'key=sucker',
+            target = self.points,
+            question = 'How much should the reward be worth (return for 2)? ')
+        self.option_set.add_option('sucker', ['s'], int, default = 0, action = 'key=sucker',
+            target = self.points,
+            question = 'How much should the temptation be worth (return for 3)? ')
