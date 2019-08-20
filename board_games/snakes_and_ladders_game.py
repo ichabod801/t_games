a185bf120700b8a10a52f85da77f88cbbda4124d
Craig O'Brien <ichabod801@xenomind.com>
2019-08-20 10:54:26 -0400

    Issue #305: Added bots to SnakesAndLadders, plus some output changes.

--- board_games/snakes_and_ladders_game.py
+++ board_games/snakes_and_ladders_game.py
@@ -9,6 +9,103 @@ from .. import dice
 from .. import board
 from .. import game
 from .. import player
+from .. import utility
+
+
+class SnakeBot(player.Bot):
+
+    def ask(self, prompt):
+        if prompt.startswith('\nPress enter'):
+            return ''
+        elif prompt.startswith('What symbol'):
+            return random.choice('!@#$%^&*<>?')
+
+    def tell(self, *args, **kwargs):
+        if not (isinstance(args[0], SnakeBoard) or args[0].startswith('That symbol')):
+            super(SnakeBot, self).tell(*args, **kwargs)
+
+
+class SnakeBoard(board.LineBoard):
+
+    layouts = {'Nepalese': [(9, 8), (10, 23), (16, 4), (17, 69), (20, 32), (24, 7), (27, 41), (28, 50),
+        (29, 6), (37, 66), (44, 9), (45, 67), (46, 62), (52, 35), (54, 66), (55, 3), (61, 13), (63, 2),
+        (72, 51)],
+        'Milton-Bradley': [(10, 10), (1, 38), (4, 14), (9, 31), (16, 6), (21, 42), (28, 84), (36, 44),
+        (47, 26), (49, 11), (51, 67), (56, 53), (62, 19), (64, 60), (71, 91), (80, 100), (87, 24), (93, 73),
+        (95, 75), (98, 78)]}
+
+    def __init__(self, name = 'Milton-Bradley', columns = 0, rows = 0):
+        """
+        Set up the board. (None)
+
+        Parameters:
+        name: The name of the layout. (str)
+        columns: The number of columns in the layout. (int)
+        rows: The number of rows in the layout. (int)
+        """
+        self.die = dice.Die()
+        if name == 'Random':
+            layout = self.random_layout(columns, rows)
+        else:
+            layout = self.layouts[name]
+        self.columns, self.rows = layout[0]
+        super(SnakeBoard, self).__init__(self.columns * self.rows, extra_cells = [0])
+        self.snakes, self.ladders = {}, {}
+        for start, end in layout[1:]:
+            if start < end:
+                self.ladders[start] = end
+                self.cells[start].add_piece('L')
+            else:
+                self.snakes[start] = end
+                self.cells[start].add_piece('S')
+
+    def __str__(self):
+        """Human readable text representation. (str)"""
+        number_text, piece_text = [''], ['']
+        for index in range(1, self.columns * self.rows + 1):
+            number_text.append('{:^4}'.format(index))
+            piece_text.append('{:^4}'.format(''.join(self.cells[index].contents)))
+        lines = ['']
+        for column in range(self.columns - 1, -1, -1):
+            #lines.append('')
+            row_order = range(self.rows, 0, -1) if column % 2 else range(1, self.rows + 1)
+            numbers, pieces = [], []
+            for row in row_order:
+                n = column * self.rows + row
+                numbers.append(number_text[n])
+                pieces.append(piece_text[n])
+            lines.append(' '.join(pieces))
+            lines.append(' '.join(numbers))
+        return '\n'.join(lines)
+
+    def roll(self, player, location, piece):
+        """
+        Roll the dice and move the player. (int)
+
+        The return value is the new location of the player.
+
+        Parameters:
+        player: The player being moved. (player.Player)
+        player: Where the player is on the board. (int)
+        piece: The player's piece symbol. (str)
+        """
+        self.cells[location].remove_piece(piece)
+        roll = self.die.roll()
+        player.tell('You rolled a {}.'.format(roll))
+        end = location + roll
+        if end > self.columns * self.rows:
+            end = self.columns * self.rows
+        player.tell('You move to square #{}.'.format(end))
+        if end in self.ladders:
+            new_end = self.ladders[end]
+            player.tell('Square #{} is a ladder up to square #{}.'.format(end, new_end))
+            end = new_end
+        if end in self.snakes:
+            new_end = self.snakes[end]
+            player.tell('Square #{} is a snake down to square #{}.'.format(end, new_end))
+            end = new_end
+        self.cells[end].add_piece(piece)
+        return end
 
 
 class SnakesAndLadders(game.Game):
@@ -64,6 +161,14 @@ class SnakesAndLadders(game.Game):
         else:
             return False
 
+    def handle_options(self):
+        """Handle the options for this play of the game. (None)"""
+        super(SnakesAndLadders, self).handle_options()
+        taken_names = [self.human.name]
+        for bot in range(self.n_bots):
+            self.players.append(SnakeBot(taken_names = taken_names))
+            taken_names.append(self.players[-1].name)
+
     def player_action(self, player):
         """
         Handle a player's turn or other player actions. (bool)
@@ -80,13 +185,18 @@ class SnakesAndLadders(game.Game):
         else:
             return self.handle_cmd(command)
 
+    def set_options(self):
+        """Define the options for the game."""
+        self.option_set.add_option('bots', ['b'], int, 3, valid = range(1, 12), target = 'n_bots',
+            question = 'How many bots should there be in the game (1-11, return for 3)? ')
+
     def set_up(self):
         """Set up the game."""
         # Set up the board and player tracking.
         self.board = SnakeBoard()
         # Get the symbols for the players' pieces.
         self.pieces = {}
-        taken_pieces = set()
+        taken_pieces = set('SL')
         for player in self.players:
             while True:
                 # As for the symbol.
@@ -100,86 +210,3 @@ class SnakesAndLadders(game.Game):
                     taken_pieces.add(piece[0])
                     self.board.cells[0].add_piece(piece[0])
                     break
-
-
-class SnakeBoard(board.LineBoard):
-
-    layouts = {'Nepalese': [(9, 8), (10, 23), (16, 4), (17, 69), (20, 32), (24, 7), (27, 41), (28, 50),
-        (29, 6), (37, 66), (44, 9), (45, 67), (46, 62), (52, 35), (54, 66), (55, 3), (61, 13), (63, 2),
-        (72, 51)],
-        'Milton-Bradley': [(10, 10), (1, 38), (4, 14), (9, 31), (16, 6), (21, 42), (28, 84), (36, 44),
-        (47, 26), (49, 11), (51, 67), (56, 53), (62, 19), (64, 60), (71, 91), (80, 100), (87, 24), (93, 73),
-        (95, 75), (98, 78)]}
-
-    def __init__(self, name = 'Milton-Bradley', columns = 0, rows = 0):
-        """
-        Set up the board. (None)
-
-        Parameters:
-        name: The name of the layout. (str)
-        columns: The number of columns in the layout. (int)
-        rows: The number of rows in the layout. (int)
-        """
-        self.die = dice.Die()
-        if name == 'Random':
-            layout = self.random_layout(columns, rows)
-        else:
-            layout = self.layouts[name]
-        self.columns, self.rows = layout[0]
-        super(SnakeBoard, self).__init__(self.columns * self.rows, extra_cells = [0])
-        self.snakes, self.ladders = {}, {}
-        for start, end in layout[1:]:
-            if start < end:
-                self.ladders[start] = end
-                self.cells[start].add_piece('L')
-            else:
-                self.snakes[start] = end
-                self.cells[start].add_piece('S')
-
-    def __str__(self):
-        """Human readable text representation. (str)"""
-        number_text, piece_text = [''], ['']
-        for index in range(1, self.columns * self.rows + 1):
-            number_text.append('{:^4}'.format(index))
-            piece_text.append('{:^4}'.format(''.join(self.cells[index].contents)))
-        lines = []
-        for column in range(self.columns - 1, -1, -1):
-            #lines.append('')
-            row_order = range(self.rows, 0, -1) if column % 2 else range(1, self.rows + 1)
-            numbers, pieces = [], []
-            for row in row_order:
-                n = column * self.rows + row
-                numbers.append(number_text[n])
-                pieces.append(piece_text[n])
-            lines.append(' '.join(pieces))
-            lines.append(' '.join(numbers))
-        return '\n'.join(lines)
-
-    def roll(self, player, location, piece):
-        """
-        Roll the dice and move the player. (int)
-
-        The return value is the new location of the player.
-
-        Parameters:
-        player: The player being moved. (player.Player)
-        player: Where the player is on the board. (int)
-        piece: The player's piece symbol. (str)
-        """
-        self.cells[location].remove_piece(piece)
-        roll = self.die.roll()
-        player.tell('You rolled a {}.'.format(roll))
-        end = location + roll
-        if end > self.columns * self.rows:
-            end = self.columns * self.rows
-        player.tell('You move to square #{}.'.format(end))
-        if end in self.ladders:
-            new_end = self.ladders[end]
-            player.tell('Square #{} is a ladder up to square #{}.'.format(end, new_end))
-            end = new_end
-        if end in self.snakes:
-            new_end = self.snakes[end]
-            player.tell('Square #{} is a snakes down to square #{}.'.format(end, new_end))
-            end = new_end
-        self.cells[end].add_piece(piece)
-        return end
