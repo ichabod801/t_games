59863038d35e5cc409bfad90986ec4259bd0a6bd
Craig O'Brien <ichabod801@xenomind.com>
2019-11-26 09:20:02 -0500

    Issue #375: Set the standard play in SmeartBot.defend to face up.
    Fixes problem when converting it back to string.

--- card_games/hearts_game.py
+++ card_games/hearts_game.py
@@ -334,6 +334,7 @@ class SmeartBot(HeartBot):
         standard = super(SmeartBot, self).play()
         if isinstance(standard, str):
             standard = cards.Card(*standard)
+            standard.up = True
         base_check = standard.suit == 'H' or standard in self.danger_cards
         if base_check or (self.game.joker_points and standard.rank == 'X'):
             suit_cards = [card for card in self.game.trick if card.suit == self.game.trick.cards[0].suit]
