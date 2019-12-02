c53eb7a6d61e03a2af382138c6d5c20f3607f986
Craig O'Brien <ichabod801@xenomind.com>
2019-12-02 10:10:47 -0500

    Issue #384: Updated copyright for the main code files.

--- full_credits.py
+++ full_credits.py
@@ -3,7 +3,7 @@ full_credits.py
 
 Generate the full credits for tgames.
 
-Copyright (C) 2018 by Craig O'Brien and the t_games contributors.
+Copyright (C) 2018-2020 by Craig O'Brien and the t_games contributors.
 See the top level __init__.py file for details on the t_games license.
 
 Constants:
