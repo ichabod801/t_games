122ec48b5a8d461a04abe35b5ac191cb14ad41a2
Craig O'Brien <ichabod801@xenomind.com>
2020-03-18 13:11:56 -0400

    Issue #495: Added a default return for Pool.counts with no dice.

--- dice.py
+++ dice.py
@@ -659,9 +659,12 @@ class Pool(object):
         rolled. Will raise an error for non-integer dice, and may be sparse depending
         on the value of self.sides.
         """
-        counts = [0] * (max(self.dice[0].sides)  + 1)
-        for value in self.values:
-            counts[value] += 1
+        if self.dice:
+            counts = [0] * (max(self.dice[0].sides)  + 1)
+            for value in self.values:
+                counts[value] += 1
+        else:
+            counts = [0] * 7
         return counts
 
     def describe(self):
