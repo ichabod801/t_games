fea35ed01ae8fa3910061534520afd8aec9d3193
Craig O'Brien <ichabod801@xenomind.com>
2020-04-20 18:48:29 -0400

    Issue #518: Clarified the game status after an edge (Number Guess).

--- other_games/number_guess_game.py
+++ other_games/number_guess_game.py
@@ -233,6 +233,12 @@ class NumberGuess(game.Game):
         # Otherwise I'm confused.
         else:
             self.human.tell("\nGipf is inside the innermost possible secret number.")
+            return True
+        self.human.tell('\nYour last guess was {}.'.format(self.last_guess))
+        if self.last_guess < self.number:
+            self.human.tell('That was lower than the secret number.')
+        else:
+            self.human.tell('That was higher than the secret number.')
         return True
 
     def do_guess(self, arguments):
