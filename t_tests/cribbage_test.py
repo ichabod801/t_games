6c92f66061fdabcf1d18aee0b60ac000f1b537d0
Craig O'Brien <ichabod801@xenomind.com>
2020-04-27 16:18:59 -0400

    Issue #579: Finished cleaning up cribbage_test.py.

--- t_tests/cribbage_test.py
+++ t_tests/cribbage_test.py
@@ -39,210 +39,211 @@ class ScoreHandTest(unittest.TestCase):
     """Test scoring cribbage hands. (unittest.TestCase)"""
 
     def setUp(self):
-        self.human = unitility.AutoBot()
-        self.game = crib.Cribbage(self.human, 'none')
+        self.human = unitility.AutoBot(['n'])
+        self.game = crib.Cribbage(self.human, 'no-cut no-pick')
+        self.game.set_up()
 
     def testFifteenDouble(self):
         """Test scoring 15s with a card working double."""
-        hand = [crib.CribCard(*card) for card in ('5S', '4S', '6D', '3H', 'KC')]
+        hand = self.game.deck.parse_text('5S 4S 6D 3H KC')
         self.assertEqual(2, self.game.score_fifteens(hand))
 
     def testFifteenFive(self):
         """Test scoring 15 with five cards."""
-        hand = [crib.CribCard(*card) for card in ('AS', '4S', '2D', '3H', '5C')]
+        hand = self.game.deck.parse_text('AS 4S 2D 3H 5C')
         self.assertEqual(1, self.game.score_fifteens(hand))
 
     def testFifteenMax(self):
         """Test scoring four 15s."""
-        hand = [crib.CribCard(*card) for card in ('5S', 'TS', 'QD', 'JH', 'KC')]
+        hand = self.game.deck.parse_text('5S TS QD JH KC')
         self.assertEqual(4, self.game.score_fifteens(hand))
 
     def testFifteenPair(self):
         """Test scoring 15 with two cards."""
-        hand = [crib.CribCard(*card) for card in ('AS', '3S', '5S', '8S', 'JS')]
+        hand = self.game.deck.parse_text('AS 3S 5S 8S JS')
         self.assertEqual(1, self.game.score_fifteens(hand))
 
     def testFifteenQuad(self):
         """Test scoring 15 with four cards."""
-        hand = [crib.CribCard(*card) for card in ('AS', '4S', '2D', '3H', '7C')]
+        hand = self.game.deck.parse_text('AS 4S 2D 3H 7C')
         self.assertEqual(1, self.game.score_fifteens(hand))
 
     def testFifteenTen(self):
         """Test scoring 15 with a face card and a five."""
-        hand = [crib.CribCard(*card) for card in ('2S', '5S', '2D', '7H', 'QC')]
+        hand = self.game.deck.parse_text('2S 5S 2D 7H QC')
         self.assertEqual(1, self.game.score_fifteens(hand))
 
     def testFifteenTrip(self):
         """Test scoring 15 with three cards."""
-        hand = [crib.CribCard(*card) for card in ('AS', '4S', '5S', '6H', '7C')]
+        hand = self.game.deck.parse_text('AS 4S 5S 6H 7C')
         self.assertEqual(1, self.game.score_fifteens(hand))
 
     def testFifteenTwice(self):
         """Test scoring 15s with two sets of cards."""
-        hand = [crib.CribCard(*card) for card in ('5S', '4S', '8H', '3H', 'KC')]
+        hand = self.game.deck.parse_text('5S 4S 8H 3H KC')
         self.assertEqual(2, self.game.score_fifteens(hand))
 
     def testFlushFive(self):
         """Test a five card flush in a hand."""
-        hand = [crib.CribCard(*card) for card in ('AS', '3S', '5S', '8S')]
-        self.game.starter = crib.CribCard(*'JS')
+        hand = self.game.deck.parse_text('AS 3S 5S 8S')
+        self.game.starter = self.game.deck.parse_text('JS')
         self.game.hands = {'The Crib': []}
         self.assertEqual(5, self.game.score_flush(hand))
 
     def testFlushFiveCrib(self):
         """Test scoring a five card flush in the crib."""
-        hand = [crib.CribCard(*card) for card in ('AS', '3S', '5S', '8S')]
-        self.game.starter = crib.CribCard(*'JS')
+        hand = self.game.deck.parse_text('AS 3S 5S 8S')
+        self.game.starter = self.game.deck.parse_text('JS')
         self.game.hands = {'The Crib': hand}
         self.assertEqual(5, self.game.score_flush(hand))
 
     def testFlushFour(self):
         """Test a four card flush in a hand."""
-        hand = [crib.CribCard(*card) for card in ('AS', '3S', '5S', '8S')]
-        self.game.starter = crib.CribCard(*'JC')
+        hand = self.game.deck.parse_text('AS 3S 5S 8S')
+        self.game.starter = self.game.deck.parse_text('JC')
         self.game.hands = {'The Crib': []}
         self.assertEqual(4, self.game.score_flush(hand))
 
     def testFlushFourCrib(self):
         """Test not scoring a four card flush in the crib."""
-        hand = [crib.CribCard(*card) for card in ('AS', '3S', '5S', '8S')]
-        self.game.starter = crib.CribCard(*'JC')
+        hand = self.game.deck.parse_text('AS 3S 5S 8S')
+        self.game.starter = self.game.deck.parse_text('JC')
         self.game.hands = {'The Crib': hand}
         self.assertEqual(0, self.game.score_flush(hand))
 
     def testFlushFourNot(self):
         """Test not scoring a four card flush made with the starter card."""
-        hand = [crib.CribCard(*card) for card in ('AS', '3C', '5S', '8S')]
-        self.game.starter = crib.CribCard(*'JS')
+        hand = self.game.deck.parse_text('AS 3C 5S 8S')
+        self.game.starter = self.game.deck.parse_text('JS')
         self.game.hands = {'The Crib': []}
         self.assertEqual(0, self.game.score_flush(hand))
 
     def testMaximum(self):
         """Test scoring the best possible hand)."""
-        hand = [crib.CribCard(*card) for card in ('5D', '5C', '5H', 'JS')]
-        self.game.starter = crib.CribCard(*'5S')
+        hand = self.game.deck.parse_text('5D 5C 5H JS')
+        self.game.starter = self.game.deck.parse_text('5S')
         self.game.double_pairs = False
         self.assertEqual(29, self.game.score_one_hand(hand, 'Bob'))
 
     def testNob(self):
         """Test scoring his nob (jack of the starter suit)."""
-        hand = [crib.CribCard(*card) for card in ('AS', '3C', '7S', 'JS')]
-        self.game.starter = crib.CribCard(*'9S')
+        hand = self.game.deck.parse_text('AS 3C 7S JS')
+        self.game.starter = self.game.deck.parse_text('9S')
         self.game.double_pairs = False
         self.assertEqual(1, self.game.score_one_hand(hand, 'Bob'))
 
     def testNothing(self):
         """Test scoring a worthless hand."""
-        hand = [crib.CribCard(*card) for card in ('4S', '7C', '9S', 'QS')]
-        self.game.starter = crib.CribCard(*'TS')
+        hand = self.game.deck.parse_text('4S 7C 9S QS')
+        self.game.starter = self.game.deck.parse_text('TS')
         self.game.double_pairs = False
         self.assertEqual(0, self.game.score_one_hand(hand, 'Bob'))
 
     def testPairDouble(self):
         """Test scoring three of a kind with doubled pairs."""
-        hand = [crib.CribCard(*card) for card in ('AD', '3C', '8H', '8S', '8C')]
+        hand = self.game.deck.parse_text('AD 3C 8H 8S 8C')
         self.game.double_pairs = True
         self.assertEqual([('8', 3, 12)], self.game.score_pairs(hand))
 
     def testPairFour(self):
         """Test scoring four of a kind."""
-        hand = [crib.CribCard(*card) for card in ('8D', '3C', '8H', '8S', '8C')]
+        hand = self.game.deck.parse_text('8D 3C 8H 8S 8C')
         self.game.double_pairs = False
         self.assertEqual([('8', 4, 12)], self.game.score_pairs(hand))
 
     def testPairFull(self):
         """Test scoring a full house."""
-        hand = [crib.CribCard(*card) for card in ('3D', '3C', '8H', '8S', '8C')]
+        hand = self.game.deck.parse_text('3D 3C 8H 8S 8C')
         self.game.double_pairs = False
         self.assertEqual(set([('8', 3, 6), ('3', 2, 2)]), set(self.game.score_pairs(hand)))
 
     def testPairNot(self):
         """Test scoring no pairs."""
-        hand = [crib.CribCard(*card) for card in ('AD', '3C', '5H', 'JS', '8C')]
+        hand = self.game.deck.parse_text('AD 3C 5H JS 8C')
         self.game.double_pairs = False
         self.assertEqual([], self.game.score_pairs(hand))
 
     def testPairOne(self):
         """Test scoring one pair."""
-        hand = [crib.CribCard(*card) for card in ('AD', '3C', '5H', '8S', '8C')]
+        hand = self.game.deck.parse_text('AD 3C 5H 8S 8C')
         self.game.double_pairs = False
         self.assertEqual([('8', 2, 2)], self.game.score_pairs(hand))
 
     def testPairThree(self):
         """Test scoring three of a kind."""
-        hand = [crib.CribCard(*card) for card in ('AD', '3C', '8H', '8S', '8C')]
+        hand = self.game.deck.parse_text('AD 5C 8H 8S 8C')
         self.game.double_pairs = False
         self.assertEqual([('8', 3, 6)], self.game.score_pairs(hand))
 
     def testPairTwo(self):
         """Test scoring two distinct pairs."""
-        hand = [crib.CribCard(*card) for card in ('AD', '5C', '5H', '8S', '8C')]
+        hand = self.game.deck.parse_text('AD 5C 5H 8S 8C')
         self.game.double_pairs = False
         self.assertEqual(set([('5', 2, 2), ('8', 2, 2)]), set(self.game.score_pairs(hand)))
 
     def testRunFive(self):
         """Test scoring a five card run."""
-        hand = [crib.CribCard(*card) for card in ('8D', '5C', '6H', '7S', '9C')]
+        hand = self.game.deck.parse_text('8D 5C 6H 7S 9C')
         self.assertEqual([(5, 1)], self.game.score_runs(hand))
 
     def testRunFour(self):
         """Test scoring a four card run."""
-        hand = [crib.CribCard(*card) for card in ('8D', '6C', '5H', '7S', 'JC')]
+        hand = self.game.deck.parse_text('8D 6C 5H 7S JC')
         self.assertEqual([(4, 1)], self.game.score_runs(hand))
 
     def testRunFourTwo(self):
         """Test scoring a four card run twice."""
-        hand = [crib.CribCard(*card) for card in ('8D', '6C', '5H', '7S', '5C')]
+        hand = self.game.deck.parse_text('8D 6C 5H 7S 5C')
         self.assertEqual([(4, 2)], self.game.score_runs(hand))
 
     def testRunFourFour(self):
         """Test scoring a four card run four times."""
-        hand = [crib.CribCard(*card) for card in ('8D', '6C', '5H', '7S', '5C', '8S')]
+        hand = self.game.deck.parse_text('8D 6C 5H 7S 5C 8S')
         self.assertEqual([(4, 4)], self.game.score_runs(hand))
 
     def testRunThree(self):
         """Test scoring a three card run."""
-        hand = [crib.CribCard(*card) for card in ('5C', 'AD', '6H', '9C', '7S')]
+        hand = self.game.deck.parse_text('5C AD 6H 9C 7S')
         self.assertEqual([(3, 1)], self.game.score_runs(hand))
 
     def testRunThreeEight(self):
         """Test scoring a three card run eight times."""
-        hand = [crib.CribCard(*card) for card in ('5C', '7D', '6H', '6C', '7S', '5D')]
+        hand = self.game.deck.parse_text('5C 7D 6H 6C 7S 5D')
         self.assertEqual([(3, 8)], self.game.score_runs(hand))
 
     def testRunThreeFour(self):
         """Test scoring a three card run four times."""
-        hand = [crib.CribCard(*card) for card in ('7H', '5C', '7D', '6H', '7C', '7S')]
+        hand = self.game.deck.parse_text('7H 5C 7D 6H 7C 7S')
         self.assertEqual([(3, 4)], self.game.score_runs(hand))
 
     def testRunThreeSix(self):
         """Test scoring a three card run six times."""
-        hand = [crib.CribCard(*card) for card in ('5C', '7D', '6H', '6C', '7S', '7H')]
+        hand = self.game.deck.parse_text('5C 7D 6H 6C 7S 7H')
         self.assertEqual([(3, 6)], self.game.score_runs(hand))
 
     def testRunThreeTwice(self):
         """Test scoring two three card runs."""
-        hand = [crib.CribCard(*card) for card in ('5C', '7D', 'TH', '6C', 'QS', 'JH')]
+        hand = self.game.deck.parse_text('5C 7D TH 6C QS JH')
         self.assertEqual([(3, 1), (3, 1)], self.game.score_runs(hand))
 
     def testRunTwo(self):
         """Test not scoring a two card run."""
-        hand = [crib.CribCard(*card) for card in ('5C', '2D', 'TH', '6C', 'QS', '8H')]
+        hand = self.game.deck.parse_text('5C 2D TH 6C QS 8H')
         self.assertEqual([], self.game.score_runs(hand))
 
     def testRunTwoTwoHigh(self):
         """Test not scoring a two card run two times on the high side."""
-        hand = [crib.CribCard(*card) for card in ('5C', '2D', 'TH', '6C', '6S', '8H')]
+        hand = self.game.deck.parse_text('5C 2D TH 6C 6S 8H')
         self.assertEqual([], self.game.score_runs(hand))
 
     def testRunTwoTwoLow(self):
         """Test not scoring a two card run two times on the low side."""
-        hand = [crib.CribCard(*card) for card in ('5C', '2D', 'TH', '6C', 'QS', '5H')]
+        hand = self.game.deck.parse_text('5C 2D TH 6C QS 5H')
         self.assertEqual([], self.game.score_runs(hand))
 
     def testRunTwoTwice(self):
         """Test not scoring two two card run."""
-        hand = [crib.CribCard(*card) for card in ('5C', '2D', 'TH', '6C', 'QS', '9H')]
+        hand = self.game.deck.parse_text('5C 2D TH 6C QS 9H')
         self.assertEqual([], self.game.score_runs(hand))
 
 
